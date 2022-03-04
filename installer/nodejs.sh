44e1589227d1560bffd82162396b8ec7a455678c
fanghao <fanghao@didichuxing.com>
2022-03-04 14:17:36 +0800

    初始化

--- installer/nodejs.sh
+++ installer/nodejs.sh
@@ -31,11 +31,15 @@ echo "Downloading $pathVersion stable Version $VER..."
 URL=http://nodejs.org/dist/$pathVersion/$VER
 FILE_PATH=/tmp/node.tar.gz
 
+https://nodejs.org/dist/v6.7.0/node-v6.7.0-linux-x64.tar.gz
 
-
-curl -o $FILE_PATH $URL 2>/dev/null
+curl -O '/tmp/node.tar.gz' 'https://nodejs.org/dist/v6.7.0/node-v6.7.0-linux-x64.tar.gz'
+curl -O $FILE_PATH $URL 2>/dev/null
 
 cd /usr/local && sudo tar --strip-components 1 -xzf /tmp/node.tar.gz
 
 #sudo ln -s /usr/local/node/bin/node /usr/bin/node
 #sudo ln -s /usr/local/node/bin/npm /usr/bin/npm
+
+sudo ln -s /home/xiaoju/node/node-v6.7.0-linux-x64/bin/yarn /usr/bin/yarn
+sudo ln -s /home/xiaoju/node/node-v6.7.0-linux-x64/bin/yarnpkg /usr/bin/yarnpkg
